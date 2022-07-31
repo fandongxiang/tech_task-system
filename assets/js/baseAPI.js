12d45e2b02dd9ec6947009a9384b39b3047eed66
fandongxiang <730596730@qq.com>
2022-08-01 07:21:24 +0800

    text

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -20,8 +20,8 @@ $.ajaxPrefilter(function (options) {
 // 添加 url 前置函数，每次调用$.get() $.post() $.ajax() 都会先调用这个
 $.ajaxPrefilter(function (options) {
   // options.url = 'http://10.4.6.132:3008' + options.url                 // 此处不能写本地 127.0.0.1 不然手机端访问不到
-  // options.url = 'http://192.168.1.14:3008' + options.url
-  options.url = 'http://192.168.1.24:3008' + options.url
+  // options.url = 'http://192.168.1.14:3008' + options.url               // 测试
+  options.url = 'http://192.168.1.124:3008' + options.url
 
   // 全局统一挂载 complete ，实现权限拦截
   options.complete = function (res) {
