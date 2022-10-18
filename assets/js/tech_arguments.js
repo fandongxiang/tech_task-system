f62421b9ea4facbd893f1049758f67969c848ad2
fandongxiang <730596730@qq.com>
2022-10-18 17:23:40 +0800

    add(compare seedRotation and cruRotaion line)

--- assets/js/tech_arguments.js
+++ assets/js/tech_arguments.js
@@ -197,8 +197,8 @@ $(function() {
         let compare_id = $(this).attr(ele_data);
         let compare_value = $(this).html();
         if (id == compare_id && value != compare_value) {
-          $(this).css('backgroundColor', 'red')
-          $length.css('backgroundColor', 'green')
+          $(this).css('backgroundColor', 'rgb(245, 120, 118)')
+          $length.css('backgroundColor', 'rgb(10, 189, 144)')
         } else if (id == compare_id && value == compare_value) {
           $(this).css('backgroundColor', '')
           $length.css('backgroundColor', '')
@@ -209,10 +209,15 @@ $(function() {
 
   // 功率-拉速 对比曲线函数
   function compareHeaterAndSlspeed() {
+    // 获取参数数组函数
+    function getArgumentsArr(element, arr) {
+      $(`.${element}`).each((index, ele) => arr.push(parseFloat(ele.innerHTML)))
+    }
+
     // 获取长度并集
     let currentLengthArr = []
     let compareLengthArr = []
-    $('.length').each((index, ele) => currentLengthArr.push(parseInt(ele.innerHTML)));
+    $('.length').each((index, ele) => currentLengthArr.push(parseFloat(ele.innerHTML)));
     $('.compare_length').each((index, ele) => compareLengthArr.push(parseInt(ele.innerHTML)));
     let unionLength = [...new Set([...currentLengthArr, ...compareLengthArr])].sort((a, b) => a < b ? -1 : a > b ? 1 : 0);
 
@@ -228,45 +233,62 @@ $(function() {
     $('.slSpeed').each((index, ele) => currentSlspeed.push(parseInt(ele.innerHTML)));
     $('.compare_slSpeed').each((index, ele) => compareSlspeed.push(parseInt(ele.innerHTML)));
 
+    // 获取晶转参数
+    let currentSeedRotation = [];
+    let compareSeedRotation = [];
+    getArgumentsArr('seedRotation', currentSeedRotation)
+    getArgumentsArr('compare_seedRotation', compareSeedRotation)
+
+    // 获取埚转参数
+    let currentCruRotation = [];
+    let compareCruRotation = [];
+    getArgumentsArr('cruRotation', currentCruRotation)
+    getArgumentsArr('compare_cruRotation', compareCruRotation)
+
     // 参数处理函数
     function getNewValue(unionLength, currentLength, currentXXX, index) {
       return Math.floor(((currentXXX[index] - currentXXX[index - 1]) / (currentLength[index] - currentLength[index - 1]) * (unionLength[index] - currentLength[index - 1]) + currentXXX[index - 1]) * 10) / 10
     }
-
     unionLength.forEach((ele, index) => {
       if (currentLengthArr.indexOf(ele) == -1) {
         let newCurrentHeat = getNewValue(unionLength, currentLengthArr, currentHeatsArr, index);
         let newCurrentSlspeed = getNewValue(unionLength, currentLengthArr, currentSlspeed, index);
+        let newCurrentSeedRotation = getNewValue(unionLength, currentLengthArr, currentSeedRotation, index);
+        let newCurrentCruRotation = getNewValue(unionLength, currentLengthArr, currentCruRotation, index);
+        currentLengthArr.splice(index, 0, unionLength[index])
         currentHeatsArr.splice(index, 0, newCurrentHeat)
         currentSlspeed.splice(index, 0, newCurrentSlspeed)
-        currentLengthArr.splice(index, 0, unionLength[index])
+        currentSeedRotation.splice(index, 0, newCurrentSeedRotation)
+        currentCruRotation.splice(index, 0, newCurrentCruRotation)
       }
       if (compareLengthArr.indexOf(ele) == -1) {
         let newCompareHeat = getNewValue(unionLength, compareLengthArr, compareHeatsArr, index);
         let newCompareSlspeed = getNewValue(unionLength, compareLengthArr, compareSlspeed, index);
+        let newCompareSeedRotation = getNewValue(unionLength, compareLengthArr, compareSeedRotation, index);
+        let newCompareCruRotation = getNewValue(unionLength, compareLengthArr, compareCruRotation, index);
+        compareLengthArr.splice(index, 0, unionLength[index])
         compareHeatsArr.splice(index, 0, newCompareHeat)
         compareSlspeed.splice(index, 0, newCompareSlspeed)
-        compareLengthArr.splice(index, 0, unionLength[index])
+        compareSeedRotation.splice(index, 0, newCompareSeedRotation)
+        compareCruRotation.splice(index, 0, newCompareCruRotation)
       }
-
     })
-    console.log(currentHeatsArr);
-    console.log(currentSlspeed);
-    console.log(compareHeatsArr);
-    console.log(compareSlspeed);
 
-    var chartDom = document.getElementById('compareLine');
-    var myChart = echarts.init(chartDom);
-    var option;
+    console.log(currentSeedRotation);
+    console.log(compareSeedRotation);
 
-    option = {
+    // 功率 - 拉速对比曲线
+    let chartDom_HeatSpeed = document.getElementById('compareHeatSpeedLine');
+    let myChart_HeatSpeed = echarts.init(chartDom_HeatSpeed);
+    let option_HeatSpeed;
+    option_HeatSpeed = {
       title: {
         text: '功率-拉速曲线对比图',
         left: 'center',
         top: 10,
-        borderColor: 'blank',
-        borderWidth: 1,
-        borderRadius: [5, 5, 5, 5]
+        // borderColor: 'blank',
+        // borderWidth: 1,
+        // borderRadius: [5, 5, 5, 5]
       },
       legend: {
         left: 'right',
@@ -274,10 +296,26 @@ $(function() {
         orient: 'vertical',
       },
       grid: {
-        left: '5 %',
-        right: '15 %',
-        top: '11 %',
+        left: '6 %',
+        right: '13 %',
+        top: '12 %',
         bottom: '8 %'
+      },
+      // 悬浮配置
+      tooltip: {
+        trigger: 'axis'
+      },
+      toolbox: {
+        show: true,
+        feature: {
+          dataZoom: {
+            yAxisIndex: 'none'
+          },
+          dataView: { readOnly: false },
+          magicType: { type: ['line', 'bar'] },
+          restore: {},
+          saveAsImage: {}
+        }
       },
       xAxis: {
         type: 'category',
@@ -287,7 +325,7 @@ $(function() {
           alignWithLabel: true,
         },
         // x 轴线与网格线对齐
-        // boundaryGap: false,
+        boundaryGap: false,
         splitLine: {
           show: true,
           lineStyle: {
@@ -298,6 +336,8 @@ $(function() {
       },
       yAxis: [{
           name: '拉速(mm/h)',
+          nameLocation: 'center',
+          nameGap: 30,
           type: 'value',
           // y 轴自适应
           // min: function(value) {
@@ -324,6 +364,8 @@ $(function() {
         {
           name: '功率(Kw)',
           type: 'value',
+          nameLocation: 'end',
+          nameGap: 7,
           // y 轴自适应
           min: 5,
           max: -5,
@@ -345,32 +387,189 @@ $(function() {
         }
       ],
       series: [{
-          name: '当前拉速曲线',
+          name: '当前拉速',
           type: 'line',
           yAxisIndex: 0,
           data: currentSlspeed,
+          label: {
+            show: true,
+            position: 'top',
+          }
         },
         {
-          name: '对比拉速曲线',
+          name: '对比拉速',
           type: 'line',
           yAxisIndex: 0,
           data: compareSlspeed,
         },
         {
-          name: '当前功率曲线',
+          name: '当前功率',
           type: 'line',
           yAxisIndex: 1,
           data: currentHeatsArr,
+          lineStyle: {
+            type: 'dashed'
+          },
+          label: {
+            show: true,
+            position: 'top',
+          }
         },
         {
-          name: '对比功率曲线',
+          name: '对比功率',
           type: 'line',
           yAxisIndex: 1,
           data: compareHeatsArr,
+          lineStyle: {
+            type: 'dashed'
+          }
         }
       ]
     };
+    option_HeatSpeed && myChart_HeatSpeed.setOption(option_HeatSpeed);
 
-    option && myChart.setOption(option);
+    // 晶 - 埚转对比曲线
+    let chartDom_SeedCruRotation = document.getElementById('compareRotationLine');
+    let myChart_SeedCruRotation = echarts.init(chartDom_SeedCruRotation);
+    let option_SeedCruRotation;
+    option_SeedCruRotation = {
+      title: {
+        text: '晶转-埚转对比曲线',
+        left: 'center',
+        top: 10,
+      },
+      legend: {
+        left: 'right',
+        top: 'middle',
+        orient: 'vertical',
+      },
+      grid: {
+        left: '6 %',
+        right: '13 %',
+        top: '12 %',
+        bottom: '8 %'
+      },
+      // 悬浮配置
+      tooltip: {
+        trigger: 'axis'
+      },
+      toolbox: {
+        show: true,
+        feature: {
+          dataZoom: {
+            yAxisIndex: 'none'
+          },
+          dataView: { readOnly: false },
+          magicType: { type: ['line', 'bar'] },
+          restore: {},
+          saveAsImage: {}
+        }
+      },
+      xAxis: {
+        type: 'category',
+        data: unionLength,
+        axisTick: {
+          // 强制坐标值与刻度对齐
+          alignWithLabel: true,
+        },
+        // x 轴线与网格线对齐
+        boundaryGap: false,
+        splitLine: {
+          show: true,
+          lineStyle: {
+            type: 'dashed',
+            color: '#55b9b4'
+          }
+        }
+      },
+      yAxis: [{
+          name: '晶转(rpm)',
+          nameLocation: 'center',
+          nameGap: 30,
+          type: 'value',
+          // y 轴自适应
+          // min: function(value) {
+          //   return value.min - 10
+          // },
+          min: 7,
+          max: 12,
+          // y 轴分割段数
+          splitNumber: 10,
+          // y 轴线显示
+          axisLine: {
+            show: true,
+          },
+          splitLine: {
+            show: true,
+            lineStyle: {
+              type: 'dashed',
+              color: '#55b9b4'
+            }
+          }
+        },
+        {
+          name: '埚转(rpm)',
+          type: 'value',
+          nameLocation: 'end',
+          nameGap: 7,
+          // y 轴自适应
+          min: 5,
+          max: 10,
+          // y 轴分割段数
+          splitNumber: 10,
+          // y 轴线显示
+          axisLine: {
+            show: true,
+          },
+          splitLine: {
+            show: true,
+            lineStyle: {
+              type: 'dashed',
+              color: '#55b9b4'
+            }
+          }
+        }
+      ],
+      series: [{
+          name: '当前晶转',
+          type: 'line',
+          yAxisIndex: 0,
+          data: currentSeedRotation,
+          label: {
+            show: true,
+            position: 'top',
+          }
+        },
+        {
+          name: '对比晶转',
+          type: 'line',
+          yAxisIndex: 0,
+          data: compareSeedRotation,
+        },
+        {
+          name: '当前埚转',
+          type: 'line',
+          yAxisIndex: 1,
+          data: currentCruRotation,
+          lineStyle: {
+            type: 'dashed'
+          },
+          label: {
+            show: true,
+            position: 'top',
+          }
+        },
+        {
+          name: '对比埚转',
+          type: 'line',
+          yAxisIndex: 1,
+          data: compareCruRotation,
+          lineStyle: {
+            type: 'dashed'
+          }
+        }
+      ]
+    }
+    option_SeedCruRotation && myChart_SeedCruRotation.setOption(option_SeedCruRotation);
   }
 })
